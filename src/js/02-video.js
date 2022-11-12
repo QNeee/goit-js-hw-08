b77cdccc73f11e68d91651a3e5cf5b863a0a60e9
QNeee <vovasagan7@gmail.com>
2022-11-12 04:15:18 +0300

    end

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,34 @@
+import throttle from 'lodash.throttle';
 import '../css/common.css';
 import Player from '@vimeo/player';
 const iframe = document.querySelector("iframe");
 const player = new Player(iframe);
-const time = "videoplayer-current-time";
-const onReloadPage = (e) => {
-    if (location.reload) {
-        onpause;
+const TIME_KEY = "videoplayer-current-time";
+const onPlay = function (d) {
+    const strigifyData = JSON.stringify(d);
+    localStorage.setItem(TIME_KEY, strigifyData);
+};
+function resumePlay() {
+    if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
+        return;
     }
-    const onPlay = data => {
+    const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
+    if (paused) {
+        player.setCurrentTime(paused).then(function (seconds) {
+            // seconds = the actual time that the player seeked to
+        }).catch(function (error) {
+            switch (error.name) {
+                case 'RangeError':
+                    // the time was less than 0 or greater than the video’s duration
+                    break;
 
-        onpause: () => window.addEventListener("player.pause", onReloadPage);
-
-    };
+                default:
+                    // some other error occurred
+                    break;
+            }
+        });
+    }
 }
 
+player.on('timeupdate', throttle(onPlay, 1000));
+resumePlay();
